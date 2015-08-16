1acc2d7825a236e5d6edc3415e6111c90dd295b7
De1mos <de1m0s242@gmail.com>
2015-08-16 19:36:46 +0700

    add bootstrap

--- Client/app/components/event/event-services.js
+++ Client/app/components/event/event-services.js
@@ -4,14 +4,18 @@ angular.module('EventServices', [])
 
 	.service('EventosService', function () {
 		var eventosList =  [
-	        {name: 'Байкал', startDate: '2015-07-19', finishDate: '2015-07-26'},
-	        {name: 'Хакатон', startDate: '2015-08-09'}
+	        {id: 1, name: 'Байкал', startDate: '2015-07-19', finishDate: '2015-07-26'},
+	        {id: 2, name: 'Хакатон', startDate: '2015-08-09'}
 	      ];
 		return {
 			getEventos : function () {
 				return eventosList;
 			},
 			addEvento : function (evento) {
+				var maxId = eventosList.reduce(function(a, b, c) { 
+					return a > b.id ? a: b.id
+				}, 0);
+				evento.id = maxId+1;
 				eventosList.push(evento);
 			}
 		};
